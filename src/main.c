04391c7de278d91318f79a555df3241d244f3880
hyiker <hyiker@bupt.edu.cn>
2021-04-06 23:11:25 +0800

    add basic framework

--- src/main.c
+++ src/main.c
@@ -1,6 +1,49 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define UDP_BUFFER_SIZE 1024
+#define UDP_PORT 8080
 int main(int argc, char const *argv[]) {
-    printf("Hello World");
+    int sockfd;
+    char buffer[UDP_BUFFER_SIZE];
+    struct sockaddr_in servaddr, cliaddr;
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("error creating socket");
+        exit(EXIT_FAILURE);
+    }
+    const char *hello = "Hello World -- Through the internet!";
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;  // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(UDP_PORT);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) <
+        0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int len, n;
+
+    len = sizeof(cliaddr);  // len is value/resuslt
+
+    n = recvfrom(sockfd, (char *)buffer, UDP_BUFFER_SIZE, MSG_WAITALL,
+                 (struct sockaddr *)&cliaddr, &len);
+    buffer[n] = '\0';
+    printf("Client : %s\n", buffer);
+    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
+           (const struct sockaddr *)&cliaddr, len);
+    printf("Hello message sent.\n");
+
     return 0;
 }
---
+++ src/message.h
@@ -0,0 +1,31 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+#include <stdint.h>
+struct message_header_t {
+    uint16_t id;
+    uint8_t misc1,  // qr, opcode, aa, tc, rd
+        misc2;      // ra, z, rcode
+    uint16_t qdcount, ancount, nscount, arcount;
+};
+struct message_question_t {
+    uint8_t* qname;  // reserve for unicode
+    uint16_t qtype;
+    uint16_t qclass;
+};
+struct resource_record {
+    uint8_t* name;
+    uint16_t type;
+    uint16_t rdata_class;
+    uint32_t ttl;
+    uint16_t rdlength;
+    uint8_t* rdate;  // specific format depends on type & class
+};
+struct message {
+    struct message_header_t header;
+    struct message_question_t* question;  // len by qdcount
+    struct resource_record* answer;       // len by ancount
+    struct resource_record* authority;    // len by nscount
+    struct resource_record* addition;     // len by arcount
+};
+
+#endif
